b01deb56f9454e772d35d10bb735d631b0c471b2
TJ <thaddeusjiang@gmail.com>
2023-11-06 12:22:15 +0900

    refactor: const

--- src/utils/long.util.ts
+++ src/utils/long.util.ts
@@ -1,11 +1,11 @@
 import Long from 'long';
 
+const POSTGRES_MAX_BIGINT = BigInt('9223372036854775807');
+const POSTGRES_MIN_BIGINT = BigInt('-9223372036854775808');
+
 export class LongUtil {
   public static fromBigInt(num: bigint) {
-    if (
-      num > BigInt('9223372036854775807') ||
-      num < BigInt('-9223372036854775808')
-    ) {
+    if (num > POSTGRES_MAX_BIGINT || num < POSTGRES_MIN_BIGINT) {
       return undefined;
     }
     return Long.fromString(num.toString());
