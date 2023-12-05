36d69edd46adcad3ddcb5034e34543c2d254a2b8
TJ <thaddeusjiang@gmail.com>
2023-12-05 18:20:54 +0900

    feat: 🎸 ProtobufUtil Date <-> Timestamp

--- src/utils/protobuf.util.ts
+++ src/utils/protobuf.util.ts
@@ -1,4 +1,5 @@
 import Long from 'long';
+import { Timestamp } from 'google-protobuf/google/protobuf/timestamp_pb';
 
 const POSTGRES_MAX_BIGINT = BigInt('9223372036854775807');
 const POSTGRES_MIN_BIGINT = BigInt('-9223372036854775808');
@@ -29,5 +30,35 @@ export class ProtobufUtil {
    */
   public static getBigIntFromInt64(id: Long) {
     return BigInt(id.toString());
+  }
+
+  /**
+   * Date -> Timestamp
+   *
+   * @param date Date
+   */
+  public static dateToTimestamp(date: Date): Timestamp;
+  public static dateToTimestamp(date: Date | undefined): Timestamp | undefined;
+  public static dateToTimestamp(date: Date | undefined): Timestamp | undefined {
+    if (!date) return undefined;
+
+    const seconds = Long.fromNumber(date.getTime() / 1_000);
+    const nanos = (date.getTime() % 1_000) * 1_000_000;
+    return { seconds, nanos };
+  }
+
+  /**
+   * Timestamp -> Date
+   *
+   * @param t Timestamp
+   */
+  public static timestampToDate(t: Timestamp): Date;
+  public static timestampToDate(t: Timestamp | undefined): Date | undefined;
+  public static timestampToDate(t: Timestamp | undefined): Date | undefined {
+    if (!t) return undefined;
+
+    let millis = (t.seconds.toNumber() || 0) * 1_000;
+    millis += (t.nanos || 0) / 1_000_000;
+    return new Date(millis);
   }
 }
