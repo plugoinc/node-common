643a920109da844460fc5729d9055ab0a231b7d3
Shingo Chijiiwa <shingo.pad@gmail.com>
2024-05-10 09:56:43 +0900

    Revert "add some function to JSONUtil"
    This reverts commit a35208ecbcffeb3c3850ff1b666002294fc91e38.

--- src/utils/json.util.ts
+++ src/utils/json.util.ts
@@ -15,26 +15,4 @@ export class JSONUtil {
       return v;
     });
   }
-
-  /**
-   * Convert JSON object to string-able object.
-   *
-   * @param data
-   * @returns
-   */
-  public static toStringable(
-    data: Record<string, any>,
-  ): Record<string, unknown> {
-    const _data = Object.assign({}, data);
-    Object.keys(_data).forEach((k) => {
-      if (typeof _data[k] === 'bigint') {
-        _data[k] = _data[k].toString();
-      }
-    });
-    return _data;
-  }
-
-  public static parse() {
-    return JSON.parse;
-  }
 }
